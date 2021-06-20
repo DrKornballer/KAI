e88e9878e0914747dac71a07bdb87b1a288b0115
DrKornballer <korn.kevin@gmail.com>
2021-06-20 10:20:34 -0700

    forms1

--- pack/views.py
+++ pack/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .forms import packsForm
 
 # Create your views here.
+
+def packs_create(request):
+     form=packsForm(request.POST or None)
+     if form.is_valid():
+          form.save()
+     return render(request, 'pack/index.htmnl')
 
 def pack(request):
      return render(request, 'pack/index.html')
